319161b469027255d50713b3d437bde51ca93667
Skorde19 <163666509+Skorde19@users.noreply.github.com>
2024-04-13 16:04:44 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,84 @@
+<?php 
+
+session_start();
+if(isset($_POST["login_btn"]))
+{
+
+$connect = mysqli_connect("localhost","root","","project3");
+
+$un = $_POST["username"];
+$pwd = md5($_POST["password"]);
+
+
+$qry ="SELECT * FROM `login` WHERE username ='$un' AND password ='$pwd' ";
+
+$result =mysqli_query($connect, $qry);
+$row = mysqli_num_rows($result) 
+
+	if($row == 1) 
+	{
+      
+        $data = mysqli_fetch_assoc($result);
+        
+        
+        $_SESSION['username'] = $data['username'];
+        
+        echo "Welcome, " . $data['username'];
+    } 
+    else {
+        echo "Something went wrong.";
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 mx-auto">
+				<div class="card">
+					<div class="card-header bg-primary d-flex justify-content-between align-items-center">
+						<h3>Login form</h3>
+						<button class="close" >&times;</button>
+
+					</div>
+					<div class="card-body">
+						<form method="post">
+							<div class="form-group">
+								<label>username</label>
+								<input type="text" name="username" placeholder="username" class="form-control">	
+							</div>
+							
+							
+							<div class="form-group">
+								<label>Password</label>
+								<input type="password" name="password" placeholder="password" class="form-control">	
+							</div>
+							
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary" name="login_btn" >login</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
